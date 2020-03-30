2e1fb1c581bf0105a035c5fa2f3641ad4522da87
Isabella Vieira <isabellavieira57@gmail.com>
2020-03-30 17:08:45 -0400

    adding node server (url hardcoded for the moment)

--- node_server/server.js
+++ node_server/server.js
@@ -1,15 +1,38 @@
-// Requirement: npm install website-scraper
-
-
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');
 const scrape = require('website-scraper');
 
-let options = {
-    urls: ['https://nodejs.org/'],
-    directory: './node-homepage',
-};
+const hostname = '127.0.0.1';
+const port = 3000;
 
-scrape(options).then((result) => {
-    console.log("Website succesfully downloaded");
-}).catch((err) => {
-    console.log("An error ocurred", err);
+var server = http.createServer(function(request, response) {  
+    var path = 'node-homepage/index.html'
+    switch (path) {   
+        case 'node-homepage/index.html':  
+            //fs.readFile(__dirname + path, function(error, data) {  
+            fs.readFile(path, function(error, data) {  
+                console.log(`Path: ${path}`)
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});  
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
