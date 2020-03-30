3e6037092da6231e200244d9ecf29e37c242a4ab
Isabella Vieira <isabellavieira57@gmail.com>
2020-03-30 17:09:37 -0400

    renaming script to download website

---
+++ node_server/download-website.js
@@ -0,0 +1,15 @@
+// Requirement: npm install website-scraper
+
+
+const scrape = require('website-scraper');
+
+let options = {
+    urls: ['https://nodejs.org/'],
+    directory: './node-homepage',
+};
+
+scrape(options).then((result) => {
+    console.log("Website succesfully downloaded");
+}).catch((err) => {
+    console.log("An error ocurred", err);
+});
